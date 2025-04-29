dec50cfd9a5c903a3a70db595d1c1bc5de3e70b9
riban <riban@zynthian.org>
2025-04-29 15:16:05 +0100

    Small doc typo

--- firmware/rmcore/plugins/src/filter.cpp
+++ firmware/rmcore/plugins/src/filter.cpp
@@ -6,7 +6,7 @@
     riban modular is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
     You should have received a copy of the GNU General Public License along with riban modular. If not, see <https://www.gnu.org/licenses/>.
 
-    Biquad 2nd order low-break filter class implementation.
+    Biquad 2nd order filter class implementation.
 */
 
 #include "filter.h"
